9fb8a78f66561f9d233e3a4b30d7ebd51a73d51e
Sjoerd van Roden <ps220419@summacollege.nl>
2022-06-16 11:40:56 +0200

    added get all oefeningen. added logging for req.

--- Laravel/summaMoveAPI/routes/api.php
+++ Laravel/summaMoveAPI/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OefeningenController;
+use App\Http\Controllers\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('oefenings', OefeningenController::class)
+    ->parameters(['oefenings' => 'oefeningen'])->only(['index']);

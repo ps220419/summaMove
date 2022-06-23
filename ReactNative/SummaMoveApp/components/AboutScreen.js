981e3ba61675c516cebd5532d70e3625240b66ba
Sjoerd van Roden <ps220419@summacollege.nl>
2022-06-23 09:53:20 +0200

    about screen finished

--- ReactNative/SummaMoveApp/components/AboutScreen.js
+++ ReactNative/SummaMoveApp/components/AboutScreen.js
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useState, } from 'react';
 
 const AboutScreen = () => {
+
+  const [versie, setVersie] = useState(1.001)
+
   return (
     <View style={styles.container}>
-      <Text>AboutScreen</Text>
+      <Text style={{flex: 3, marginTop: "20%", width: "70%"}}>Summa Move is fitness app voor studenten bij Summa College. In deze app kunnen gebruikers diverse oefeningen bekijken met een foto en uitleg. ingelogde gebruikers kunnen hun eigen prestaties zien en nieuwe prestaties toevoegen.</Text>
+      <Text style={{flex: 1}}>Hulp nodig? neem contact op: +31 62465672</Text>
+      <Text style={{ marginBottom: "0%"}}>versie: {versie}</Text>
     </View>
+    
   )
 }
 
@@ -17,5 +24,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        padding: "5%",
       },
 })
